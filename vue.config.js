89ed0aa6dc3f287c7731ef648dcb0588a8f8cb04
rmb <nnn111>
2020-11-09 11:09:43 +0800

    初始化 项目

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports  = {
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {  //代理
+      '/api': {
+        target:'https://www.imooc.com',// 目标，代理到哪里
+        changeOrigin: true,  //是否要将主机头的原点，更改为url的地址
+        pathRewrite: {  // 转发地址
+          '/api': ''
+        }
+      }
+    }
+  }
+}
